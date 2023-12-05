831649ecb03ddd61229555d5050135bc697d55b9
IsaacManzo <isaacemanzo51@gmail.com>
2023-12-05 11:31:27 -0300

    first repo

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,9 @@
+// react-scroll
+import { Link } from "react-scroll";
+//
+
+const NavLink = ({ children, to, offset = -50, mobileMenu = false }) => {
+  return <div>NavLink</div>;
+};
+
+export default NavLink;
